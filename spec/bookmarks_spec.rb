58c6993a8736411c65cfcf0aa0cff7515714ee70
Nora van den Heuvel <noravdheuvel@gmail.com>
2020-03-10 16:33:23 +0000

    refactored test

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -2,11 +2,10 @@ require 'bookmarks'
 
 describe Bookmarks do
   it "can return a list of bookmarks" do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+    Bookmarks.create(url: "http://www.makersacademy.com")
+    Bookmarks.create(url: "http://www.destroyallsoftware.com")
+    Bookmarks.create(url: "http://www.google.com")
 
     bookmarks = Bookmarks.all
 
